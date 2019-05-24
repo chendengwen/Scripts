df68f3188a21692c4e3e28a1c1a6725827c8f90e
Gary <596765672@qq.com>
2019-05-24 16:33:31 +0800

    。。。

--- Swift/SwiftGuided.playground/Pages/Error Handling.xcplaygroundpage/Contents.swift
+++ Swift/SwiftGuided.playground/Pages/Error Handling.xcplaygroundpage/Contents.swift
@@ -21,7 +21,9 @@ func send(job: Int, toPrinter printerName: String) throws -> String {
     return "Job sent"
 }
 
-//: 有多种方式可以用来进行错误处理。一种方式是使用 do-catch 。在 do 代码块中，使用 try 来标记可以抛出错误的代码。在 catch 代码块中，除非你另外命名，否则错误会自动命名为 error 。
+//: > Swift 中有4种处理错误的方式。你可以把函数抛出的错误传递给调用此函数的代码、用do-catch语句处理错误、将错误作为可选类型处理、或者断言此错误根本不会发生。
+
+//:在 do 代码块中，使用 try 来标记可以抛出错误的代码。在 catch 代码块中，除非你另外命名，否则错误会自动命名为 error 。
 //:
 do {
     let printerResponse = try send(job: 1040, toPrinter: "Bi Sheng")
@@ -48,13 +50,13 @@ do {
 
 //: - Experiment:
 //: 在 do 代码块中添加抛出错误的代码。你需要抛出哪种错误来使第一个 catch 块进行接收？怎么使第二个和第三个 catch 进行接收呢？
-//:
+
 //: 另一种处理错误的方式使用 try? 将结果转换为可选的。如果函数抛出错误，该错误会被抛弃并且结果为 nil。否则的话，结果会是一个包含函数返回值的可选值。
 //:
 let printerSuccess = try? send(job: 1884, toPrinter: "Mergenthaler")
 let printerFailure = try? send(job: 1885, toPrinter: "Never Has Toner")
 
-//: 使用 defer 代码块来表示在函数返回前，函数中最后执行的代码。无论函数是否会抛出错误，这段代码都将执行。使用 defer，可以把函数调用之初就要执行的代码和函数调用结束时的扫尾代码写在一起，虽然这两者的执行时机截然不同。
+//: 使用 defer 代码块来表示在函数返回前，函数中最后执行的代码。无论函数是否会抛出错误，或是由于诸如return、break的语句，这段代码都将执行。使用 defer，可以把函数调用之初就要执行的代码和函数调用结束时的扫尾代码写在一起，虽然这两者的执行时机截然不同。
 //:
 var fridgeIsOpen = false
 let fridgeContent = ["milk", "eggs", "leftovers"]
--- Swift/SwiftGuided.playground/Pages/Generics.xcplaygroundpage/Contents.swift
+++ Swift/SwiftGuided.playground/Pages/Generics.xcplaygroundpage/Contents.swift
@@ -44,4 +44,31 @@ anyCommonElements([1, 2, 3], [3])
 //:
 
 
+//:Any 和 AnyObject 的类型转换
+//:>Swift 为不确定类型提供了两种特殊的类型别名：
+//:- Any 可以表示任何类型，包括函数类型。
+//:- AnyObject 可以表示任何类类型的实例。
+//示例，使用 Any 类型来和混合的不同类型一起工作，包括函数类型和非类类型。它创建了一个可以存储 Any 类型的数组 things：
+class Person { var name: String? }
+var person = Person()
+person.name = "Ghostbusters"
+
+var things = [Any]()
+things.append(0)
+things.append(0.0)
+things.append(42)
+things.append(3.14159)
+things.append("hello")
+things.append((3.0, 5.0))
+things.append(person)
+things.append({ (name: String) -> String in "Hello, \(name)" })
+
+
+//Any类型可以表示所有类型的值，包括可选类型。
+//Swift会在你用Any类型来表示一个可选值的时候，给你一个警告。
+//如果你确实想使用Any类型来承载可选值，你可以使用as操作符显式转换为Any，如下所示：
+let optionalNumber: Int? = 3
+things.append(optionalNumber)        // 警告
+things.append(optionalNumber as Any) // 没有警告
+
 //: [Previous](@previous) | [Next](@next)
